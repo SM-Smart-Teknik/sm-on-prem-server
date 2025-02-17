4e13c152f666881d4ee3ffbc4c4666d7d3518a9a
Lucas Strand <lucas.strand@outlook.com>
2025-02-17 14:28:17 +0100

    syncs all users

--- MicrosoftGraph/config.js
+++ MicrosoftGraph/config.js
@@ -1,12 +1,20 @@
 const USEREMAILMAP = {
+  "Aron Fransson": "aron.fransson@smartteknik.nu",
+  "Elin Svensson": "elin.svensson@smartteknik.nu",
+  "Emil Smådahl": "emil.smadahl@smartteknik.nu",
+  "Johan Hermansson": "johan.hermansson@smartteknik.nu",
+  "Josef Hammarsand": "josef.hammarsand@smartteknik.nu",
   "Lucas Strand": "lucas.strand@smartteknik.nu",
-  // Add all users and their email addresses
+  "Marthin Marelius": "marthin.marelius@smartteknik.nu",
+  "Sam Sköld": "sam.skold@smartteknik.nu",
+  "Timi Adamer": "timi.adamer@smartteknik.nu",
 };
+
 module.exports = USEREMAILMAP;
 
 // const USEREMAILMAP = {
 //   "Aron Fransson": "aron.fransson@smartteknik.nu",
-//   "Elin Svensson": "elin.svensson@smartteknik.nu",
+//   "Elin Svensson": "elin.svensson@smartteknik.nu"d,
 //   "Emil Smådahl": "emil.smadahl@smartteknik.nu",
 //   "Johan Hermansson": "johan.hermansson@smartteknik.nu",
 //   "Josef Hammarsand": "josef.hammarsand@smartteknik.nu",
